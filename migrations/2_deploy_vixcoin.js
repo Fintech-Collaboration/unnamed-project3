3915b16d343a11c38cae7179addc6725f38192db
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-12-01 22:41:24 -0800

    Cleaned out for dev-jasmine merge.

--- migrations/2_deploy_vixcoin.js
+++
@@ -1,9 +0,0 @@
-const VixcoinToken = artifacts.require("VixcoinToken");
-
-module.exports = function (deployer) {
-  const _name = "Vixcoin";
-  const _symbol = "VXCN";
-  const _decimals = 18;
-
-  deployer.deploy(VixcoinToken, _name, _symbol, _decimals);
-};
